0589fe665bc94aca0f245b4ec91f07e2080fb10f
Bartłomiej Mitek <bartekmitek@wp.pl>
2020-08-31 23:38:45 +0200

    File BankManager acomplished

---
+++ udacity-java-course/object/lesson4/TheBankManager/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+G
+src/SavingsAccount.java,6/8/68ba904ef39482e8704470016a3e5259bd66a292
+M
+src/CertificateOfDeposit.java,d/5/d52021ac3c3a630238e57e90bbbccd082fa8814a
+D
+src/BankAccount.java,d/c/dc27c071375c64151b6a3035f9d9b43b3323e6dd
+H
+src/CheckingAccount.java,f/9/f93251f39a130c861b5529b0bcd2495576f97ffd
+=
+src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
--- udacity-java-course/object/lesson4/TheBankManager/src/CertificateOfDeposit.java
+++ udacity-java-course/object/lesson4/TheBankManager/src/CertificateOfDeposit.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class CertificateOfDeposit {
+public class CertificateOfDeposit extends BankAccount {
+    String expiry;
+
+    public void ChangeExpiryDate(String newExpiry) {
+        expiry = newExpiry;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
 }
